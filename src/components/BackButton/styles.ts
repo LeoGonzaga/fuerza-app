6b7f66c9074bc3eadd493e7410ee0a9b447fa787
LeoGonzaga <sacredlotus05@gmail.com>
2022-09-13 20:17:32 -0300

    feat:creating back component

---
+++ src/components/BackButton/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  padding: 0px 22px;
+
+  display: flex;
+  align-items: center;
+
+  button {
+    background-color: transparent;
+    border: none;
+    margin-right: 19px;
+  }
+`;
+
+export const Styles = {
+  Container,
+};
---
+++ src/pages/Posts/CreatePost/CreatePost.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BackButton from '../../../components/BackButton';
+import Header from '../../../components/Header';
+import Template from '../../../components/Template';
+
+export const CreatePost = (): JSX.Element => {
+  return (
+    <Template>
+      <Header />
+      <BackButton />
+    </Template>
+  );
+};
